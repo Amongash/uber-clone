51b87c5caabe3e2fac74e1029b2caf4e2f062743
amongash <amongash08@gmail.com>
2021-08-18 13:49:47 +0300

    Removed flipper

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,11 +1,11 @@
 import Reactotron from "reactotron-react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import ReactotronFlipper from "reactotron-react-native/dist/flipper";
+// import ReactotronFlipper from "reactotron-react-native/dist/flipper";
 
 Reactotron.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
 	.configure({
 		name: "Uber Clone",
-		createSocket: (path) => new ReactotronFlipper(path),
+		// createSocket: (path) => new ReactotronFlipper(path),
 	}) // controls connection & communication settings
 	.useReactNative() // add all built-in react native plugins
 	.connect(); // let's connect!
